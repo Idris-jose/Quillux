39715ab58a5735446e6958edd8c90c99e680ffab
Idris jose <idrisjose11@gmail.com>
2025-09-01 12:44:15 +0100

    probable fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   optimizeDeps: {
-    include: ["firebase/auth", "firebase/firestore", "firebase/app"]
-  }
 })
 
